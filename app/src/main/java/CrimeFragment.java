11616240c7d1502a1ac36e452fa2892e571df0e5
Allen Lee <allenlee@usc.edu>
2018-04-01 21:02:34 -0700

    CrimeFragment

---
+++ app/src/main/java/CrimeFragment.java
@@ -0,0 +1,52 @@
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.example.phorust.criminalintent.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by phorust on 4/1/18.
+ */
+
+public class CrimeFragment extends Fragment {
+    private Crime mCrime;
+    @BindView(R.id.crime_title) EditText mTitleField;
+
+    @Override
+    //notice that this onCreate is public so that it can be accessed by any activity
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState){
+        View v = inflater.inflate(R.layout.fragment_crime, parent, false);
+
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                mCrime.setTitle(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        return v;
+    }
+}
--- app/src/main/java/com/example/phorust/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/example/phorust/criminalintent/CrimeActivity.java
@@ -1,6 +1,8 @@
 package com.example.phorust.criminalintent;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -13,5 +15,15 @@ public class CrimeActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
         ButterKnife.bind(this);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null){
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
     }
 }
