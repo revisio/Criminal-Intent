57b020e17f8d467fa02c27e1c9917b710af3c4b5
Allen Lee <allenlee@usc.edu>
2018-04-04 17:51:21 -0700

    manifest change migrate launcher intent filter from CrimeActivity to CrimeListActivity

---
+++ app/src/main/java/com/example/phorust/criminalintent/CrimeListFragment.java
@@ -0,0 +1,10 @@
+package com.example.phorust.criminalintent;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by phorust on 4/4/18.
+ */
+
+public class CrimeListFragment extends Fragment{
+
+}
